b3a52811afd42e7611c7acd9b5a04c7d635994de
Henrick_0 <107651823+Henrick1@users.noreply.github.com>
2022-08-28 03:21:51 -0300

    Arquivos iniciais
    site completo

---
+++ jsarq1.js
@@ -0,0 +1,8 @@
+var confirmar = document.getElementById("data").value;
+var sla = document.getElementById("entrar").style.visibility = "hidden";
+
+function entrar() {
+    if (confirmar = "19/01") {
+    sla = document.getElementById("entrar").style.visibility = "visible";
+    }
+}
