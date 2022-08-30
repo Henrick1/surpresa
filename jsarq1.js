cf1eb6dc87ebdea9ffa8f41bdc5f67633f84430f
Henrick_0 <107651823+Henrick1@users.noreply.github.com>
2022-08-30 16:34:54 -0300

    definitive
    definitive version

--- jsarq1.js
+++ jsarq1.js
@@ -1,8 +1,12 @@
-var confirmar = document.getElementById("data").value;
 var sla = document.getElementById("entrar").style.visibility = "hidden";
 
 function entrar() {
-    if (confirmar = "19/01") {
-    sla = document.getElementById("entrar").style.visibility = "visible";
+    var confirmar = document.getElementById("data").value;
+    console.log(confirmar)
+    if (confirmar == 1901) {
+        sla = document.getElementById("entrar").style.visibility = "visible";
+    }
+    else{
+        sla = document.getElementById("entrar").style.visibility = "hidden";
     }
 }
