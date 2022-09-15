67a186b25b5f69b8126297452bf192eb8e821e6b
Henrick_0 <107651823+Henrick1@users.noreply.github.com>
2022-09-14 23:34:52 -0300

    Delete jsarq3.js

--- jsarq3.js
+++
@@ -1,5 +0,0 @@
-var ocultar2 = document.getElementById("cxmsgfinal").style.visibility = "hidden";
-
-function msg() {
-    var ocultar = document.getElementById("cxmsgfinal").style.visibility = "visible";
-}
